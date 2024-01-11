ac82687d8ebd25fd6a5ba60e04441ba760f7c4ff
ela-hidri <elahidri.eh@gmail.com>
2024-01-11 12:56:49 -0800

    adding 102-type_checking.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,9 @@ duck-typed annotations function  safe_first_elemen
 from typing import Any, Mapping, Sequence, TypeVar, Union
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('T'), None] = None) -> Union[Any, TypeVar('T')]:
+def safely_get_value(dct: Mapping, key: Any, 
+                     default: Union[TypeVar('T'), None] = None
+                     )-> Union[Any, TypeVar('T')]:
     """
     safely_get_value
     """
