e471a102c359cdae979d622ee0a369b59f86a738
ela-hidri <elahidri.eh@gmail.com>
2024-01-11 12:12:19 -0800

    adding 8-make_multiplie

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annotated function  make_multiplier
+"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    to_kv function
+    """
+    def mul(x: float) -> float:
+        return x * multiplier
+    return mul
