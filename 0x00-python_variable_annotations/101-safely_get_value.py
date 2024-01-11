82878968e789059e684f8cfb57c89e7b0733a359
ela-hidri <elahidri.eh@gmail.com>
2024-01-11 12:37:01 -0800

    adding 101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+duck-typed annotations function  safe_first_elemen
+"""
+
+from typing import Any, Mapping, Sequence, TypeVar, Union
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('T'), None] = None) -> Union[Any, TypeVar('T')]:
+    """
+    safely_get_value
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
