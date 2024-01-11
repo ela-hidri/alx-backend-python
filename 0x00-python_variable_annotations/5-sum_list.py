5afbba710e95311c7e8e19dd943e0e19cc453f52
ela-hidri <elahidri.eh@gmail.com>
2024-01-11 11:46:10 -0800

    adding 5-sum_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function  to_str
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    sum_list function
+    """
+    return sum(input_list)
