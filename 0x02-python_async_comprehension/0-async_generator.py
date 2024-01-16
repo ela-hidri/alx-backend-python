913ea482a039c00119dfa9427a6298a7d843e4b5
ela-hidri <elahidri.eh@gmail.com>
2024-01-16 10:40:19 -0800

    adding 0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+The basics of  Asynchronous Comprehensions
+"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    loop 10 times, each time asynchronously wait 1 second
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
