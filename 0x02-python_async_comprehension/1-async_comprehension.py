73f31fddfef8a50b77ac93792b3b913f8cc3b9cb
ela-hidri <elahidri.eh@gmail.com>
2024-01-16 11:15:43 -0800

    adding 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,15 +4,12 @@ The basics of  Asynchronous Comprehensions
 """
 import asyncio
 import random
-from typing import Generator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     collect 10 random numbers using an async comprehensing    """
-    randomNbs = []
-    async for ran in async_generator():
-        randomNbs.append(ran)
-    return randomNbs
+    return [_ async for _ in async_generator()]
