d5688f0766ae47c52cbbe8018a26918f0e0bd5e9
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-02 21:09:17 +0100

    init files

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""testing utils.access_nested_map"""
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+
+    ])
+    def test_map(self):
+        
+
+#if __name__ == '__main__':
+    #unittest.main()
