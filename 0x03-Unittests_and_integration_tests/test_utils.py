ed59d89090d3ff6c47c1808d121bb60b20d12b7b
ela-hidri <elahidri.eh@gmail.com>
2024-02-02 21:46:58 +0000

    adding test file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,14 +2,19 @@
 """testing utils.access_nested_map"""
 import unittest
 from parameterized import parameterized
+import utils
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """testing map """
     @parameterized.expand([
-
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_map(self):
-        
+    def test_map(self, value1, val2, expected):
+        """testing"""
+        self.assertEqual(utils.access_nested_map(value1, val2), expected)
 
-#if __name__ == '__main__':
-    #unittest.main()
+if __name__ == '__main__':
+    unittest.main()
