d18ed03bdbf0ff4aebc35e7992f5bbe93825a7e1
ela-hidri <elahidri.eh@gmail.com>
2024-02-05 18:42:47 +0000

    fixing test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,18 +3,25 @@
 import unittest
 from parameterized import parameterized
 import utils
+from typing import Tuple, Union
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """testing map """
+    """
+    testing map
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_map(self, value1, val2, expected):
-        """testing"""
+    def test_map(self, value1: Dict,
+                 val2: Tuple[str], expected: Union[Dict, int]) -> None:
+        """
+        testing access_nested_map
+        """
         self.assertEqual(utils.access_nested_map(value1, val2), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
